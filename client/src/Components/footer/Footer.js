be26971b4b0610c7cb107b6870e9c40146640b63
lovert-amin <lovmomo700@gmail.com>
2020-05-14 06:49:51 -0700

    adding footer

--- client/src/Components/footer/Footer.js
+++ client/src/Components/footer/Footer.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import "./footer.css";
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="footer-container">
+                <div className="footer">
+                    <span>HOTEL LOVETECH</span>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                </div>
+                <div className="footer">
+                    <span>LATEST NEWS</span>
+                    <p>Hotel Zante in pictures
+                    10 Things You Should Know
+                    Hotel Zante Weddings
+                    Hotel Zante family party
+                    Live your myth in Greece</p>
+                </div>
+                <div className="footer">
+                    <span>USEFUL LINKS</span>
+                    <p>Location<br/>
+                    Gallery<br/>
+                    Contact us<br/>
+                    About us<br/>
+                    History</p>
+                </div>
+                <div>
+                    <span>CONTACT US</span>
+                    <p>
+                    25, Navagio Zakynthos, Greece<br/>
+                    Phone: 123-12345678<br/>
+                    Email: contact@hotelzante.com<br/>
+                    </p>
+                </div>
             </div>
         )
     }
--- client/src/Components/main/Main.js
+++ client/src/Components/main/Main.js
@@ -6,61 +6,64 @@ import Gallery from "../Gallery"
 export default class Main extends Component {
     render() {
         return (
-           <> 
-            <div className="main-container">
-                <div className="main">
-                    <h1>WELCOME T0 LOVTECH LUXURY  HOTEL</h1>
-                    <div>
-                        <p>Moderne Rooms and Speciuos Suites</p>
-                        <p>A Gourmet Dinning Experience</p>
-                        <p>Family Rooms $400 Per Night</p>
+            <>
+                <div className="main-container">
+                    <div className="main">
+                        <h1>WELCOME T0 LOVTECH LUXURY  HOTEL</h1>
+                        <div>
+                            <p>Moderne Rooms and Speciuos Suites</p>
+                            <p>A Gourmet Dinning Experience</p>
+                            <p>Family Rooms $400 Per Night</p>
+                        </div>
+                        <div className="side-bar">
+                            <form>
+                                <p>BOOK YOUR STAY</p>
+                                <label for="Check In / Out">Check in / out</label> <br />
+                                <input type="check in / out" id="check in " name="check in" /> <br />
+                                <label for="Guest">Guest 1</label> <br />
+                                <input type="check in / out" id="check in " name="check in" />
+                                <p>Check Availlability</p>
+                            </form>
+                        </div>
+                        
                     </div>
-                    <div className="side-bar">
-                        <form>
-                            <p>BOOK YOUR STAY</p>
-                            <label for="Check In / Out">Check in / out</label> <br />
-                            <input type="check in / out" id="check in " name="check in" /> <br />
-                            <label for="Guest">Guest 1</label> <br />
-                            <input type="check in / out" id="check in " name="check in" />
-                            <p>Check Availlability</p>
-                        </form>
-                    </div>
-
+                    <div className="shot-bar">
+                            <div className="shot-bar-info">
+                                <span>ENJOY FREE WIFI</span>
+                                <p>IN THE WHOLE HOTEL</p>
+                            </div>
+                            <div className="shot-bar-info">
+                                <span>CABLE TELEVISION </span>
+                                <p>FREE CABLE TV</p>
+                            </div>
+                            <div className="shot-bar-info">
+                                <span>RESTAURANT</span>
+                                <p>RESTAURANT $ BAR</p>
+                            </div>
+                        </div>
                 </div>
-                <div className="shot-bar">
-                    <div className="shot-bar-info">
-                        <span>ENJOY FREE WIFI</span>
-                        <p>IN THE WHOLE HOTEL</p>
-                    </div>
-                    <div className="shot-bar-info">
-                        <span>CABLE TELEVISION </span>
-                        <p>FREE CABLE TV</p>
-                    </div>
-                    <div className="shot-bar-info">
-                        <span>RESTAURANT</span>
-                        <p>RESTAURANT $ BAR</p>
-                    </div>
-                </div>
-            </div>
-
-            <p className="one">OUR GUESTS LOVE US</p>
-            <div className="client-info">
-                <div className="cleint">
-                <span>Nice Place</span>
-                <p>Fantastic location in a historic renovated monastery in the heart of the Castro.<br/> Staff were helpful and knowledgeable and everything in easy walking distance.</p>
-                </div>
-                <div className="cleint">
-                    <span>Perfect</span>
-                    <p>The owner and staff were extremely helpful and kind and took time to ensure<br/> that we had extra information on things to do in the area and places to visit.</p>
+                <div>
+                    <p className="one">OUR GUESTS LOVE US</p>
+                    <p className="two">Check out our awesome services</p>
                 </div>
 
-                <div className="cleint" >
-                    <span>Good Choice</span>
-                    <p>
-                    Extremely nice environment, the room was great, the service was awesome,<br/> really helpful and great service I'll visit them again in the near future</p>
+                <div className="client-info">
+                    <div className="cleint">
+                        <span>Nice Place</span>
+                        <p>Fantastic location in a historic renovated monastery in the heart of the Castro.<br /> Staff were helpful and knowledgeable and everything in easy walking distance.</p>
+                    </div>
+                    <div className="cleint">
+                        <span>Perfect</span>
+                        <p>The owner and staff were extremely helpful and kind and took time to ensure<br /> that we had extra information on things to do in the area and places to visit.</p>
+                    </div>
+
+                    <div className="cleint" >
+                        <span>Good Choice</span>
+                        <p>
+                            Extremely nice environment, the room was great, the service was awesome,<br /> really helpful and great service I'll visit them again in the near future</p>
+                    </div>
                 </div>
-            </div>
-            <Gallery/>
+                <Gallery />
             </>
         )
     }
